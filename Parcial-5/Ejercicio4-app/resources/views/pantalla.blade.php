a0aa69b318b49b0db4632e05b7bc492cf3c5def9
Eduardo Marges <135554783+How1tzer@users.noreply.github.com>
2023-11-06 17:11:18 +0000

    Ejercicio-4

---
+++ Parcial-5/Ejercicio4-app/resources/views/pantalla.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Operaciones Matemáticas</title>
+</head>
+<body>
+    @if(isset($resultado))
+        <h2>El resultado es: {{ $resultado }}</h2>
+    @endif
+
+</body>
+</html>
---
+++ Parcial-5/Ejercicio4-app/resources/views/saludo.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saludo</title>
+</head>
+<body>
+    <h2>¡Bienvenido!, {{ $nombre }} {{ $apellido }}. Un gusto que estes de vuelta </h2>
+</body>
+</html>
